899a0a8548de5934924c62e0c161fb00698e1522
Shubham Sood <41718296+ShubhamSood1406@users.noreply.github.com>
2020-06-03 20:26:33 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,12 +1,15 @@
 from datetime import datetime
-
+import pytz
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from script import msg
+#importing the callmsg function from script.py that you have made
+from script import callmsg
+
+ist = pytz.timezone('Asia/Kolkata')
 
 sched = BlockingScheduler()
 
-# Schedule job_function to be called every two hours
-sched.add_job(msg, 'interval', seconds = 60)
+# Schedule the python script to be called everyday from monday to friday at 7:17 P.M. You change the time as per your need.
+sched.add_job(callmsg, 'cron', day_of_week='mon-fri', hour=19, minute=17, timezone=ist)
 
 sched.start()
