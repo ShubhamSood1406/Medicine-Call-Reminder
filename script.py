d032c33f7b1b658e91914c3906f60ac78f312e6a
Shubham Sood <41718296+ShubhamSood1406@users.noreply.github.com>
2020-06-03 20:34:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,15 @@
-from twilio.rest import Client 
- 
-account_sid = 'Your account_sid here' 
-auth_token = 'Your auth_token here' 
-client = Client(account_sid, auth_token) 
- 
-def msg():
-    message = client.messages.create( 
-                                from_='whatsapp:+14155238886',  
-                                body='Your appointment is coming up on July 21 at 3PM',      
-                                to='whatsapp:+918285671810' 
-                            ) 
-        
-    print(message.sid)
+from twilio.rest import Client
+
+account_sid = 'AC68dd6fb818a70751f9f8a5af3235'       #Replace with your account_sid
+auth_token = '0d9c4274fea8fc95aXXXXXXXX'          #Replace with your auth_token
+
+client = Client(account_sid, auth_token)
+
+def callmsg():    
+    call = client.calls.create(
+          from_='+17146604658',     #Replace with the phone number that you got from Twilio
+          twiml='<Response><Say>Reminder to take your Pill at 7:17 P.M.</Say></Response>',
+          to='+918285XXXXXX'    #Phone number that you add on Twilio
+    )
+  
+print(call.sid)
